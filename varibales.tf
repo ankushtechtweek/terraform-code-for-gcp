274fbbe711893ea4dd64d757f6776ab8f4d3a226
DevOps <techtweek.com>
2023-04-12 19:53:24 +0530

    added

---
+++ varibales.tf
@@ -0,0 +1,19 @@
+variable "project_name" {
+
+description = "Project name"
+type = any
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  type = any
+}
+
+variable "region" {
+
+description = "Region for the enviornment"
+type = any
+}
+
+variable "secrets" {
+type = any
+}
