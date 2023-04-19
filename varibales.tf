82c71e3fa3b1e3352dd8c93767adcd73cfb28f8f
ankushtechtweek <92977536+ankushtechtweek@users.noreply.github.com>
2023-04-19 15:38:24 +0530

    Update varibales.tf

--- varibales.tf
+++ varibales.tf
@@ -63,5 +63,9 @@ variable "secrets" {
       secret_name  = "ZOOM_TOKEN_ENCRYPTIONDASDAS"
       secret_value = "my_secret_value_10"
     }
+    "ZOOM_TOKEN_ENCRYPTION_KEYASDA_NEW" = {
+      secret_name  = "ZOOM_TOKEN_ENCRYPTIONDASDAS"
+      secret_value = "my_secret_value_10"
+    }
   }
 }
