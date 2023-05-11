344f6ae410beed35381714d9b03072b05a40b36b
DevOps <techtweek.com>
2023-05-11 15:49:29 +0530

    vpc added

--- main.tf
+++ main.tf
@@ -3,8 +3,13 @@ module "secret" {
   source = "./modules/secret_manager"
   secrets       = var.secrets
 }
-*/
+
 
 module "vpc" {
   source = "./modules/vpc"
+}
+*/
+
+module "fortinet" {
+  source = "./modules/fortinet"
 }
---
+++ modules/fortinet/variable.tf
@@ -0,0 +1,60 @@
+
+##fortinet##
+
+# GCP zone
+variable "zone" {
+  type    = string
+  default = "us-east1-c" #Default Zone
+}
+
+# GCP Fortinet official project
+variable "ftntproject" {
+  type    = string
+  default = "fortigcp-project-001"
+}
+
+# FortiGate Image name
+# 7.2.4 payg is projects/fortigcp-project-001/global/images/fortinet-fgtondemand-724-20230201-001-w-license
+# 7.2.4 byol is projects/fortigcp-project-001/global/images/fortinet-fgt-724-20230201-001-w-license
+variable "image" {
+  type    = string
+  default = "projects/fortigcp-project-001/global/images/fortinet-fgtondemand-724-20230201-001-w-license"
+}
+# GCP VNIC type
+# either GVNIC or VIRTIO_NET
+variable "nictype" {
+  type    = string
+  default = "GVNIC"
+}
+# GCP instance machine type
+variable "machine" {
+  type    = string
+  default = "n1-standard-1"
+}
+# VPC CIDR
+variable "vpc_cidr" {
+  type    = string
+  default = "172.16.0.0/16"
+}
+# Public Subnet CIDR
+variable "public_subnet" {
+  type    = string
+  default = "172.16.0.0/24"
+}
+# Private Subnet CIDR
+variable "protected_subnet" {
+  type    = string
+  default = "172.16.1.0/24"
+}
+
+# user data for bootstrap fgt configuration
+variable "user_data" {
+  type    = string
+  default = "config.txt"
+}
+
+# user data for bootstrap fgt license file
+variable "license_file" {
+  type    = string
+  default = "license.lic" #FGT BYOL license
+}
